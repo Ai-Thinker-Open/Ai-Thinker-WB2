fc77037120b3373b6cf49261ddec002084773f21
SeaHi <1140920745@qq.com>
2022-10-09 15:39:24 +0800

    craete station example

---
+++ applications/socket/tcp_client/tcp_client/main.c
@@ -0,0 +1,20 @@
+/*
+ * @Author: xuhongv@yeah.net xuhongv@yeah.net
+ * @Date: 2022-10-03 15:02:19
+ * @LastEditors: xuhongv@yeah.net xuhongv@yeah.net
+ * @LastEditTime: 2022-10-08 14:55:16
+ * @FilePath: \bl_iot_sdk_for_aithinker\applications\get-started\helloworld\helloworld\main.c
+ * @Description: Hello world
+ */
+#include <stdio.h>
+#include <string.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include <blog.h>
+#include "bl_sys.h"
+
+void main(void)
+{
+
+
+}
---
+++ applications/wifi/softAP/README.md
@@ -0,0 +1,8 @@
+# hello world Example
+
+## Example Output
+
+
+## Troubleshooting
+
+For any technical queries, please open an [issue](https://github.com/Ai-Thinker-Open/Ai-Thinker-WB2/issues) on GitHub. We will get back to you soon.
