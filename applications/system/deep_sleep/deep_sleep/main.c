36cad880cd0f3fd3fbfa8f0666549163d42983e2
Chenc <1539551105@qq.com>
2022-11-25 17:11:33 +0800

    add deep sleep

---
+++ applications/system/deep_sleep/deep_sleep/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include <blog.h>
+#include "bl_sys.h"
+#include <stdint.h>
+#include <string.h>
+#include "bl_sys.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "hosal_rng.h"
+#include "hal_hbn.h"
+#include <bl_gpio.h>
+
+#define ENTER_SLEEP_PIN  4
+#define WEAKUP_PIN       7   
+#define test (*((volatile uint8_t *)0x40010000)) 
+
+void main(void)
+{
+    uint32_t sleep_time = 0;
+    uint8_t weakup_pin;
+    bl_gpio_enable_input(ENTER_SLEEP_PIN, 0, 0);
+    printf("test:%d\r\n",test);
+    vTaskDelay(pdMS_TO_TICKS(100));
+    weakup_pin=WEAKUP_PIN;
+    for (;;) {
+        if (!bl_gpio_input_get_value(ENTER_SLEEP_PIN)) {
+            printf("enter sleep\r\n");
+            test++;
+            vTaskDelay(pdMS_TO_TICKS(100));
+            hal_hbn_init(&weakup_pin, 1);
+            hal_hbn_enter((uint32_t)sleep_time);         
+        }
+        vTaskDelay(pdMS_TO_TICKS(5));
+    }
+}
