ac7974cb68bfb0dbc3956165cc697c06da385347
SeaHi-Mo <seahi@foxmail.com>
2022-12-29 11:43:48 +0800

    add lvgl label example

---
+++ applications/iot-solution/lvgl_example/lvgl_label/README.md
@@ -0,0 +1,35 @@
+# LVGL Example
+
+The display driver used in this routine is SSD1306,and the size is `128 * 64`.I2C communication mode。
+## LVGL config
+LVGL configuration is unified in the `lv_conf.h` file. You can configure the model, size and drive pin of the display,
+### Model configuration
+```c
+#define LV_DISPLAY_SSD1306
+```
+### I2C pin configuration
+```c
+#define OLED_IIC_SCL 12
+#define OLED_IIC_SDA 3
+```
+### Display direction configuration
+```c
+#define LV_DISPLAY_ORIENTATION_LANDSCAPE 
+  // #define LV_DISPLAY_ORIENTATION_LANDSCAPE_INVERTED 
+```
+- LV_DISPLAY_ORIENTATION_LANDSCAPE:Normal display
+- LV_DISPLAY_ORIENTATION_LANDSCAPE_INVERTED: Mirror Display
+
+### Display screen size configuration
+```c
+#define MY_DISP_HOR_RES    128
+#define MY_DISP_VER_RES    64
+#define LV_VER_RES_MAX     10
+```
+## Run
+
+<img src="img/ssd1306.jpg" width="50%">
+
+## Troubleshooting
+
+For any technical queries, please open an [issue](https://github.com/Ai-Thinker-Open/Ai-Thinker-WB2/issues) on GitHub. We will get back to you soon.
---
+++ applications/iot-solution/lvgl_example/lvgl_label/lvgl_label/main.c
@@ -0,0 +1,63 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include <blog.h>
+#include "bl_sys.h"
+#include "lvgl.h"
+#include "lv_conf.h"
+#include "lv_port_disp.h"
+#include <hosal_timer.h>
+
+
+static void timer_cb(void* arg)
+{
+    lv_tick_inc(1);
+}
+
+void lv_example_label_1(void)
+{
+    lv_obj_t* label1 = lv_label_create(lv_scr_act());
+    lv_label_set_long_mode(label1, LV_LABEL_LONG_WRAP);     /*Break the long lines*/
+    lv_label_set_recolor(label1, true);                      /*Enable re-coloring by commands in the text*/
+    lv_label_set_text(label1, "#0000ff Re-color# #ff00ff words# #ff0000 of a# label, align the lines to the center "
+                              "and wrap long text automatically.");
+    lv_obj_set_width(label1, 150);  /*Set smaller width to make the lines wrap*/
+    lv_obj_set_style_text_align(label1, LV_TEXT_ALIGN_CENTER, 0);
+    lv_obj_align(label1, LV_ALIGN_CENTER, 0, -40);
+
+    lv_obj_t* label2 = lv_label_create(lv_scr_act());
+    lv_label_set_long_mode(label2, LV_LABEL_LONG_SCROLL_CIRCULAR);     /*Circular scroll*/
+    lv_obj_set_width(label2, 150);
+    lv_label_set_text(label2, "It is a circularly scrolling text. ");
+    lv_obj_align(label2, LV_ALIGN_CENTER, 0, 40);
+}
+
+void main(void)
+{
+
+    static hosal_timer_dev_t lv_timer_dev = {
+        .config = {
+            .arg = NULL,
+            .cb = timer_cb,
+            .period = 1000,
+            .reload_mode = TIMER_RELOAD_PERIODIC,
+        },
+        .port = 0,
+    };
+    lv_init();
+
+    lv_port_disp_init();
+
+    hosal_timer_init(&lv_timer_dev);
+    hosal_timer_start(&lv_timer_dev);
+
+    lv_example_label_1();
+    while (1) {
+        vTaskDelay(10/portTICK_PERIOD_MS);
+        lv_timer_handler();
+    }
+}
+
+
