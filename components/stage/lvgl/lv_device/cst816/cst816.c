233589e5dbe4a98b95a6d9147f1e3098f7668c74
SeaHi-Mo <seahi@foxmail.com>
2022-12-29 17:05:03 +0800

    update lvgl components

--- components/stage/lvgl/lv_device/cst816/cst816.c
+++ components/stage/lvgl/lv_device/cst816/cst816.c
@@ -1,7 +1,8 @@
 #include "cst816.h"
 #include "hosal_i2c.h"
 #include "hosal_gpio.h"
-
+#include "lv_conf.h"
+#include "lv_port_indev.h"
 QueueHandle_t touch_point_queue;
 
 static hosal_i2c_dev_t i2c0 = {
@@ -15,7 +16,7 @@ static hosal_i2c_dev_t i2c0 = {
     .port = 0,
 };
 
-static void touch_irq_handler(void *arg)
+static void touch_irq_handler(void* arg)
 {
     touch_point_t point;
     static touch_point_t prev;
@@ -46,4 +47,30 @@ void cst816_init()
     hosal_gpio_init(&touch_gpio);
 
     hosal_gpio_irq_set(&touch_gpio, HOSAL_IRQ_TRIG_NEG_PULSE, touch_irq_handler, NULL);
+}
+
+void cst826_read(void* indev_data)
+{
+
+    lv_indev_data_t* data = (lv_indev_data_t*)indev_data;
+#if (CONFIG_LV_DISPLAY_ORIENTATION==0)
+    hosal_i2c_mem_read(&i2c0, CST816_DEFAULT_ADDR, 0x02, 1, &data->state, 1, 10);
+    hosal_i2c_mem_read(&i2c0, CST816_DEFAULT_ADDR, 0x04, 1, &data->point.y, 1, 10);
+    hosal_i2c_mem_read(&i2c0, CST816_DEFAULT_ADDR, 0x06, 1, &data->point.x, 1, 10);
+    data->point.x = 240 - data->point.x;
+#elif (CONFIG_LV_DISPLAY_ORIENTATION==1)
+    hosal_i2c_mem_read(&i2c0, CST816_DEFAULT_ADDR, 0x02, 1, &data->state, 1, 10);
+    hosal_i2c_mem_read(&i2c0, CST816_DEFAULT_ADDR, 0x04, 1, &data->point.y, 1, 10);
+    hosal_i2c_mem_read(&i2c0, CST816_DEFAULT_ADDR, 0x06, 1, &data->point.x, 1, 10);
+    data->point.y = 240-data->point.y;
+#elif (CONFIG_LV_DISPLAY_ORIENTATION==2)
+    hosal_i2c_mem_read(&i2c0, CST816_DEFAULT_ADDR, 0x02, 1, &data->state, 1, 10);
+    hosal_i2c_mem_read(&i2c0, CST816_DEFAULT_ADDR, 0x04, 1, &data->point.x, 1, 10);
+    hosal_i2c_mem_read(&i2c0, CST816_DEFAULT_ADDR, 0x06, 1, &data->point.y, 1, 10);
+
+#elif (CONFIG_LV_DISPLAY_ORIENTATION==3)
+    hosal_i2c_mem_read(&i2c0, CST816_DEFAULT_ADDR, 0x02, 1, &data->state, 1, 10);
+    hosal_i2c_mem_read(&i2c0, CST816_DEFAULT_ADDR, 0x04, 1, &data->point.y, 1, 10);
+    hosal_i2c_mem_read(&i2c0, CST816_DEFAULT_ADDR, 0x06, 1, &data->point.x, 1, 10);
+#endif
 }
