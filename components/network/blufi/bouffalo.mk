e227448ea0d0129735bc58b1e89700ee283a8187
Chenc <1539551105@qq.com>
2022-12-30 15:15:04 +0800

    update blufi lib

--- components/network/blufi/bouffalo.mk
+++ components/network/blufi/bouffalo.mk
@@ -1,12 +1,30 @@
-#
-# "main" pseudo-component makefile.
-#
-# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
-COMPONENT_LIB_ONLY := 1
-COMPONENT_ADD_INCLUDEDIRS += include
-LIBS ?= blufi
-COMPONENT_ADD_LDFLAGS += -L$(COMPONENT_PATH) $(addprefix -l,$(LIBS))
-ALL_LIB_FILES := $(patsubst %,$(COMPONENT_PATH)/lib%.a,$(LIBS))
-COMPONENT_ADD_LINKER_DEPS := $(ALL_LIB_FILES)
+# Component Makefile
+#
+include $(BL60X_SDK_PATH)/components/network/ble/ble_common.mk
 
+ifeq ($(CONFIG_ENABLE_PSM_RAM),1)
+CPPFLAGS += -DCONF_USER_ENABLE_PSRAM
+endif
+
+ifeq ($(CONFIG_ENABLE_CAMERA),1)
+CPPFLAGS += -DCONF_USER_ENABLE_CAMERA
+endif
+
+ifeq ($(CONFIG_ENABLE_BLSYNC),1)
+CPPFLAGS += -DCONF_USER_ENABLE_BLSYNC
+endif
+
+ifeq ($(CONFIG_ENABLE_VFS_SPI),1)
+CPPFLAGS += -DCONF_USER_ENABLE_VFS_SPI
+endif
+
+ifeq ($(CONFIG_ENABLE_VFS_ROMFS),1)
+CPPFLAGS += -DCONF_USER_ENABLE_VFS_ROMFS
+endif
+
+COMPONENT_ADD_INCLUDEDIRS := ./
+
+COMPONENT_OBJS := $(patsubst %.c,%.o, $(COMPONENT_SRCS))
+
+COMPONENT_SRCDIRS := ./
