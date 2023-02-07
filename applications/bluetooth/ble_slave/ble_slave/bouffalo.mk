9b8903efb58ef7d63aa4e8cffbe11b527b39c263
Chenc <1539551105@qq.com>
2023-02-07 18:26:46 +0800

    update ble slave demo

--- applications/bluetooth/ble_slave/ble_slave/bouffalo.mk
+++ applications/bluetooth/ble_slave/ble_slave/bouffalo.mk
@@ -1,3 +1,7 @@
+#
+# "main" pseudo-component makefile.
+#
+# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
 include $(BL60X_SDK_PATH)/components/network/ble/ble_common.mk
 
