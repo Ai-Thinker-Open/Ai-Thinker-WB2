966a99e5fbdadf813dc45ea7272e93ca149d1bd9
chenyy <chenyy@tech-now.com>
2023-06-29 14:58:47 +0800

    dele unnecessary blufi demo

--- applications/blufi_user/my_blufi/ble_interface.h
+++
@@ -1,9 +0,0 @@
-#ifndef BLE_INTERFACE_H
-#define BLE_INTERFACE_H
-#include "axk_ble.h"
-
-int axk_hal_ble_role_set(ble_role_t role);
-int axk_hal_ble_adv_stop(void);
-int axk_hal_ble_adv_start(ble_adv_param_t *adv_param, ble_adv_data_t *adv_data);
-
-#endif
